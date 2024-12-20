3ffee5ab30f56cf2cfa6d466a7de08ae79833773
DheerajSuthar001 <dheerajsuthar.it21@gmail.com>
2024-12-20 14:49:05 +0530

    Added dark mode feature and search container

---
+++ script.js
@@ -0,0 +1,27 @@
+// Dark Mode 
+const DLText=document.querySelector("[data-DarkLight]");
+const DLButton=document.querySelector("[btn-DLBtn]");
+const DLImage=document.querySelector("[DLImg]");
+const DarkContainers=document.querySelectorAll("[darkLight]");
+const DarkContainerCheck=document.querySelector("[darkLight]");
+
+ 
+
+DLButton.addEventListener('click',renderDarkLightElements);
+function renderDarkLightElements(){
+    if(!DarkContainerCheck.classList.contains("dark")){
+        for(const el of DarkContainers){
+            el.classList.add("dark");
+        };
+        DLText.textContent='Light';
+        DLImage.setAttribute("src","assets/sun.png")
+    }
+    else{
+        for(const el of DarkContainers){
+            el.classList.remove("dark");
+        };
+        DLText.textContent='Dark';
+        DLImage.setAttribute("src","assets/moon.png")
+    }
+    
+}
